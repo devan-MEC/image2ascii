ce8e296e0a54fec6598ba55f8a69426d4f5c9dd9
Arun Parolikkal <actuday6418@gmail.com>
2022-11-25 01:11:07 +0530

    Add support for GIFs, add some sample images

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use colored::Colorize;
-use image::imageops::FilterType;
-use image::io::Reader as ImageReader;
+use crossterm::{
+    cursor, queue,
+    style::{self, Color, Print, PrintStyledContent, Stylize},
+    terminal::{self, Clear, ClearType},
+    ExecutableCommand, QueueableCommand,
+};
+use image::AnimationDecoder;
 use image::GenericImageView;
 use image::Pixel;
+use image::{codecs::gif::GifDecoder, Frame};
+use image::{imageops::FilterType, DynamicImage};
+use image::{io::Reader as ImageReader, ImageFormat};
+use std::io::{stdout, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
 /// Simple program generate ASCII art from an input image
 #[derive(Parser, Debug)]
@@ -27,18 +37,33 @@ const HEAT_MAP: [char; 16] = [
     ' ', '.', '´', ':', 'i', '!', 'I', '~', '+', 'x', '$', 'X', '#', '▄', '■', '█',
 ];
 
-fn main() -> Result<()> {
-    let args = Args::parse();
-    let img = ImageReader::open(args.file_path)
-        .context("file_path should be a valid path to a file")?
-        .decode()
-        .context("file_path should point to an image file with the correct extension")?;
-    let img = if args.resize {
-        img.resize(100, 100, FilterType::Nearest)
+fn resize_img(img: DynamicImage) -> Result<DynamicImage> {
+    //TODO correct image resizing algortihm
+    let canvas_dimensions = terminal::size()?;
+    let canvas_dimensions = (canvas_dimensions.0 as u32, canvas_dimensions.1 as u32);
+    let img_dimensions = img.dimensions();
+    let wr = img_dimensions.0 / canvas_dimensions.0;
+    let hr = img_dimensions.1 / canvas_dimensions.1;
+    Ok(if hr > wr {
+        img.resize(
+            canvas_dimensions.1 * img_dimensions.0 / img_dimensions.1,
+            canvas_dimensions.1,
+            FilterType::Nearest,
+        )
     } else {
-        img
-    };
+        img.resize(
+            canvas_dimensions.0,
+            canvas_dimensions.0 * img_dimensions.1 / img_dimensions.0,
+            FilterType::Nearest,
+        )
+    })
+}
+
+fn print_img(img: DynamicImage, args: &Args) -> Result<()> {
+    let mut stdout = stdout();
+    let img = if args.resize { resize_img(img)? } else { img };
     let (width, height) = img.dimensions();
+    queue!(stdout, Clear(ClearType::FromCursorUp))?;
     let pixels_with_value: Vec<(u8, u8, u8, u8)> = img
         .pixels()
         .map(|p| {
@@ -54,20 +79,61 @@ fn main() -> Result<()> {
         .collect();
     for i in 0..height {
         for j in i * width..i * width + width {
-            print!(
-                "{}",
-                if args.colored {
-                    let p = pixels_with_value[j as usize];
-                    HEAT_MAP[p.3 as usize]
-                        .to_string()
-                        .truecolor(p.0, p.1, p.2)
-                        .to_string()
-                } else {
-                    HEAT_MAP[pixels_with_value[j as usize].3 as usize].to_string()
-                }
-            );
+            if args.colored {
+                let p = pixels_with_value[j as usize];
+                queue!(
+                    stdout,
+                    PrintStyledContent(HEAT_MAP[p.3 as usize].to_string().with(Color::Rgb {
+                        r: p.0,
+                        g: p.1,
+                        b: p.2
+                    }))
+                )?
+            } else {
+                queue!(
+                    stdout,
+                    Print(HEAT_MAP[pixels_with_value[j as usize].3 as usize].to_string())
+                )?
+            }
         }
-        println!("");
+        queue!(stdout, Print("\n"))?;
+    }
+    stdout.flush()?;
+    Ok(())
+}
+
+fn print_gif(path: &str, args: &Args) -> Result<()> {
+    //TODO fix scrolling issue when animating a GIF
+    let file = std::fs::File::open(path)?;
+    GifDecoder::new(file)?
+        .into_frames()
+        .collect_frames()?
+        .into_iter()
+        .map(Frame::into_buffer)
+        .map(|frame| {
+            let mut stdout = stdout();
+            stdout.execute(cursor::MoveTo(0, 0))?;
+            drop(stdout);
+            let img = DynamicImage::ImageRgba8(frame);
+            print_img(img, &args)?;
+            sleep(Duration::from_millis(300));
+            Ok::<_, anyhow::Error>(())
+        })
+        .collect()
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let path = args.file_path.clone();
+    if image::ImageFormat::from_path(&path).context("file_path should be a valid path to a file")?
+        == ImageFormat::Gif
+    {
+        print_gif(&path, &args)?;
+    } else {
+        let img = ImageReader::open(path)?
+            .decode()
+            .context("file_path should point to an image file with the correct extension")?;
+        print_img(img, &args)?;
     }
     Ok(())
 }
